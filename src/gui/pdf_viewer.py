b20d9d19f274c902ba88ba9d7a817b8148d88b6b
Kuba Sławomir Forczek <kuba.forczek.stud@pw.edu.pl>
2023-11-20 17:02:27 +0100

    GUI variables refactor (encapsulation)

--- src/gui/pdf_viewer.py
+++ src/gui/pdf_viewer.py
@@ -25,25 +25,25 @@ class PDFViewer(QtWidgets.QWidget):
         self.setGeometry(100, 100, 1160, 700)
         self.setWindowTitle('Beamer Beautifier')
 
-        self.pdf_widget = QtWidgets.QLabel(self)
-        self.pdf_widget.setAlignment(QtCore.Qt.AlignCenter)
+        self._pdf_label = QtWidgets.QLabel(self)
+        self._pdf_label.setAlignment(QtCore.Qt.AlignCenter)
 
-        self.pdf_list = QtWidgets.QListWidget(self)
-        self.pdf_list.setViewMode(QtWidgets.QListWidget.IconMode)
-        self.pdf_list.setIconSize(QtCore.QSize(460, 300))
-        self.pdf_list.setResizeMode(QtWidgets.QListWidget.Adjust)
-        self.pdf_list.setMovement(QtWidgets.QListWidget.Static)
+        self._thumbs_list = QtWidgets.QListWidget(self)
+        self._thumbs_list.setViewMode(QtWidgets.QListWidget.IconMode)
+        self._thumbs_list.setIconSize(QtCore.QSize(460, 300))
+        self._thumbs_list.setResizeMode(QtWidgets.QListWidget.Adjust)
+        self._thumbs_list.setMovement(QtWidgets.QListWidget.Static)
 
         pdf_label_container = QtWidgets.QWidget()
         pdf_label_layout = QtWidgets.QVBoxLayout()
 
         # Navigation buttons
-        self.previous_button = QtWidgets.QPushButton("←")
-        self.previous_button.clicked.connect(self._previous_page)
-        self.previous_button.setFixedSize(50, 50)
-        self.next_button = QtWidgets.QPushButton("→")
-        self.next_button.clicked.connect(self._next_page)
-        self.next_button.setFixedSize(50, 50)
+        self._prev_button = QtWidgets.QPushButton("←")
+        self._prev_button.clicked.connect(self._previous_page)
+        self._prev_button.setFixedSize(50, 50)
+        self._next_button = QtWidgets.QPushButton("→")
+        self._next_button.clicked.connect(self._next_page)
+        self._next_button.setFixedSize(50, 50)
 
         self._improve_button = QtWidgets.QPushButton("Select this version")
         self._improve_button.setEnabled(False)
@@ -51,28 +51,28 @@ class PDFViewer(QtWidgets.QWidget):
         # Button layout with spacers
         button_layout = QtWidgets.QHBoxLayout()
         button_layout.addStretch(1)
-        button_layout.addWidget(self.previous_button)
+        button_layout.addWidget(self._prev_button)
         button_layout.addSpacing(20)  # Spacing between buttons
-        button_layout.addWidget(self.next_button)
+        button_layout.addWidget(self._next_button)
         button_layout.addStretch(1)
 
         pdf_display_layout = QtWidgets.QVBoxLayout()
-        pdf_display_layout.addWidget(self.pdf_widget)
+        pdf_display_layout.addWidget(self._pdf_label)
         pdf_display_layout.addWidget(self._improve_button)
         pdf_display_layout.setAlignment(self._improve_button, QtCore.Qt.AlignRight)
-        self.pdf_widget.setMinimumWidth(250)
+        self._pdf_label.setMinimumWidth(250)
 
         pdf_label_layout.addLayout(pdf_display_layout)
         pdf_label_layout.addLayout(button_layout)
         pdf_label_container.setLayout(pdf_label_layout)
 
-        self.splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
-        self.splitter.addWidget(pdf_label_container)
-        self.splitter.addWidget(self.pdf_list)
-        self.splitter.setSizes([1040, 460])
+        self._splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
+        self._splitter.addWidget(pdf_label_container)
+        self._splitter.addWidget(self._thumbs_list)
+        self._splitter.setSizes([1040, 460])
 
         layout = QtWidgets.QHBoxLayout()
-        layout.addWidget(self.splitter)
+        layout.addWidget(self._splitter)
         self.setLayout(layout)
 
         self._display_page()
@@ -80,8 +80,8 @@ class PDFViewer(QtWidgets.QWidget):
 
     def _load_thumbnails(self):
         # TODO will be changed in future
-        while self.pdf_list.count() > 0:
-            self.pdf_list.takeItem(0)
+        while self._thumbs_list.count() > 0:
+            self._thumbs_list.takeItem(0)
 
         for idx, opt in enumerate(self._current_page):
             pixmap = opt.scaled(200, 200, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
@@ -89,7 +89,7 @@ class PDFViewer(QtWidgets.QWidget):
             item.setIcon(QtGui.QIcon(QtGui.QPixmap.fromImage(QtGui.QImage(pixmap.toImage()))))
             if idx == self._selected_opt:
                 item.setSelected(True)
-            self.pdf_list.addItem(item)
+            self._thumbs_list.addItem(item)
 
     def _previous_page(self):
         page = self._document.prev_page()
@@ -110,12 +110,12 @@ class PDFViewer(QtWidgets.QWidget):
         self._load_thumbnails()
 
     def _display_page(self):
-        current_width = self.pdf_widget.width()
-        current_height = self.pdf_widget.height()
+        current_width = self._pdf_label.width()
+        current_height = self._pdf_label.height()
         pixmap = self._current_page[self._selected_opt].scaled(
             current_width, current_height, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation
         )
-        self.pdf_widget.setPixmap(pixmap)
+        self._pdf_label.setPixmap(pixmap)
 
     def resizeEvent(self, event):
         self._display_page()
