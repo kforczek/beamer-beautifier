31432a6c7983df283a64fce601731dd746771a17
Kuba Sławomir Forczek <kuba.forczek.stud@pw.edu.pl>
2023-12-04 11:12:25 +0100

    Local thumbnails fix

--- src/gui/app.py
+++ src/gui/app.py
@@ -37,7 +37,7 @@ class MainWindow(QtWidgets.QFrame):
         splitter.left_pane.navigation_buttons.next_button.clicked.connect(self._next_page)
         self._improve_button.clicked.connect(self._select_improvement)
         self._local_thumbnails_view.itemSelectionChanged.connect(self._local_thumbnail_selection_changed)
-        # TODO connect thumbs bottom
+        self._global_thumbnails_view.itemSelectionChanged.connect(self._global_thumbnail_selection_changed)
         self._save_button.clicked.connect(self._save_changes)
 
     def _init_document_logic(self, document: BeamerDocument):
@@ -46,31 +46,27 @@ class MainWindow(QtWidgets.QFrame):
         if not page:
             raise EmptyDocumentError("The document doesn't contain any pages, got nothing to display")
 
-        self._highlighted_opt = None
+        self._local_highlighted_opt = None
+        self._global_highlighted_opt = None
         self._changes_count = 0
-        self._thumbs = []
+        self._local_thumb_items = []
+        self._global_thumb_items = []
         self._load_page(page)
 
     def _load_thumbnails(self):
-        self._load_specific_thumbnails(self._local_thumbnails_view, self._curr_local_improvements)
-        self._load_specific_thumbnails(self._global_thumbnails_view, self._curr_global_improvements)
+        self._load_specific_thumbnails(self._local_thumbnails_view, self._local_thumb_items)
+        self._load_specific_thumbnails(self._global_thumbnails_view, self._global_thumb_items)
 
-    def _load_specific_thumbnails(self, dest_listview, thumbnails_list):
-        self._thumbs.clear()
+    def _load_specific_thumbnails(self, dest_listview, thumbnail_items):
         while dest_listview.count() > 0:
             dest_listview.takeItem(0)
 
-        for idx, opt in enumerate(thumbnails_list):
-            pixmap = opt.scaled(200, 200, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
-            item = QtWidgets.QListWidgetItem()
-            item.setIcon(QtGui.QIcon(QtGui.QPixmap.fromImage(QtGui.QImage(pixmap.toImage()))))
-
+        for idx, item in enumerate(thumbnail_items):
             dest_listview.addItem(item)
-            self._thumbs.append(item)
 
             if idx == self._selected_opt:
                 dest_listview.setCurrentItem(item)
-                self._highlighted_opt = self._selected_opt
+                self._local_highlighted_opt = self._selected_opt
 
     def _prev_page(self):
         page = self._document.prev_page()
@@ -87,14 +83,21 @@ class MainWindow(QtWidgets.QFrame):
         self._load_page(page)
 
     def _load_page(self, page_info: PageInfo):
+        self._local_thumb_items.clear()
+        self._global_thumb_items.clear()
+
         self._curr_local_improvements = [to_qt_pixmap(page_opt) for page_opt in page_info.local_improvements]
+        self._local_thumb_items = [to_thumbnail_item(improvement) for improvement in self._curr_local_improvements]
+
         self._curr_global_improvements = [to_qt_pixmap(page_opt) for page_opt in page_info.global_improvements]
+        self._global_thumb_items = [to_thumbnail_item(improvement) for improvement in self._curr_global_improvements]
+
         self._selected_opt = self._document.current_frame_alternative()
         self._display_page()
         self._load_thumbnails()
 
     def _display_page(self):
-        option = self._highlighted_opt if self._highlighted_opt is not None else self._selected_opt
+        option = self._local_highlighted_opt if self._local_highlighted_opt is not None else self._selected_opt
         current_width = self._image_display.width()
         current_height = self._image_display.height()
         pixmap = self._curr_local_improvements[option].scaled(
@@ -106,29 +109,37 @@ class MainWindow(QtWidgets.QFrame):
         curr_item = self._local_thumbnails_view.currentItem()
 
         try:
-            self._highlighted_opt = self._thumbs.index(curr_item) if curr_item else None
+            self._local_highlighted_opt = self._local_thumb_items.index(curr_item) if curr_item else None
         except ValueError:
-            self._highlighted_opt = None
+            self._local_highlighted_opt = None
 
-        if self._highlighted_opt in (None, self._selected_opt):
-            if len(self._thumbs) > self._selected_opt:
-                self._local_thumbnails_view.setCurrentItem(self._thumbs[self._selected_opt])
-                self._highlighted_opt = self._selected_opt
+        if self._local_highlighted_opt in (None, self._selected_opt):
+            if len(self._local_thumb_items) > self._selected_opt:
+                self._local_thumbnails_view.setCurrentItem(self._local_thumb_items[self._selected_opt])
+                self._local_highlighted_opt = self._selected_opt
                 self._handle_thumb_highlight()
             return
 
         # Another option has been highlighted
         self._handle_thumb_highlight()
 
+    def _global_thumbnail_selection_changed(self):
+        curr_item = self._global_thumbnails_view.currentItem()
+
+        try:
+            self._global_highlighted_opt = self._local_thumb_items.index(curr_item) if curr_item else None
+        except ValueError:
+            self._global_highlighted_opt = None
+
     def _handle_thumb_highlight(self):
-        self._improve_button.setEnabled(self._highlighted_opt != self._selected_opt)
+        self._improve_button.setEnabled(self._local_highlighted_opt != self._selected_opt)
         self._display_page()
 
     def _select_improvement(self):
-        if self._highlighted_opt == self._selected_opt:
+        if self._local_highlighted_opt == self._selected_opt:
             return
 
-        self._selected_opt = self._highlighted_opt
+        self._selected_opt = self._local_highlighted_opt
         self._document.select_alternative(self._selected_opt)
         self._update_save_button_state(self._selected_opt)
         self._handle_thumb_highlight()
@@ -156,6 +167,13 @@ def to_qt_pixmap(fitz_pixmap):
     img = QtGui.QImage(fitz_pixmap.samples, fitz_pixmap.width, fitz_pixmap.height,
                        fitz_pixmap.width * fitz_pixmap.n, QtGui.QImage.Format_RGBA8888)
     return QtGui.QPixmap.fromImage(img)
+
+
+def to_thumbnail_item(qt_pixmap):
+    pixmap = qt_pixmap.scaled(200, 200, QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation)
+    item = QtWidgets.QListWidgetItem()
+    item.setIcon(QtGui.QIcon(pixmap))
+    return item
 
 
 def run_app(document: BeamerDocument, doc_folder: str):
